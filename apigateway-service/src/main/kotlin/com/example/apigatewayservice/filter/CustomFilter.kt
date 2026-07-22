9f6724e541879188af4d633bc781452f1b642e8e
kkangh00n <kig2454@gmail.com>
2026-07-22 16:24:53 +0900

    api gateway service - CustomFilter 추가 및 적용

---
+++ apigateway-service/src/main/kotlin/com/example/apigatewayservice/filter/CustomFilter.kt
@@ -0,0 +1,30 @@
+package com.example.apigatewayservice.filter
+
+import com.example.apigatewayservice.util.log
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+@Component
+class CustomFilter : AbstractGatewayFilterFactory<Any>() {
+
+    override fun apply(config: Any): GatewayFilter {
+        return GatewayFilter { exchange, chain ->
+            val request = exchange.request
+            val response = exchange.response
+
+            // Custom Pre Filter
+            log.info { "Custom PRE Filter: request id -> ${request.id}" }
+
+            chain.filter(exchange)
+                .then(
+                    Mono.fromRunnable {
+                        // Custom Post Filter
+                        log.info { "Custom POST Filter: response code -> ${response.statusCode}" }
+                    }
+                )
+        }
+    }
+
+}
