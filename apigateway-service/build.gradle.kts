c523fbb6176d84dda2ff279a28a32830aeed1aff
kkangh00n <kig2454@gmail.com>
2026-07-22 16:24:42 +0900

    api gateway service - 로깅 util 추가

--- apigateway-service/build.gradle.kts
+++ apigateway-service/build.gradle.kts
@@ -2,4 +2,7 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    //logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.4")
 }
