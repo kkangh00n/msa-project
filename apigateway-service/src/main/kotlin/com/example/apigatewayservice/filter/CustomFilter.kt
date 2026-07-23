5fe5e1ff66b73c261cc8c932dba06a12da3df394
kkangh00n <kig2454@gmail.com>
2026-07-23 11:25:21 +0900

    api gateway service - CustomFilter 순서 최상단 적용

--- apigateway-service/src/main/kotlin/com/example/apigatewayservice/filter/CustomFilter.kt
+++ apigateway-service/src/main/kotlin/com/example/apigatewayservice/filter/CustomFilter.kt
@@ -2,7 +2,9 @@ package com.example.apigatewayservice.filter
 
 import com.example.apigatewayservice.util.log
 import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.core.Ordered
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
@@ -10,7 +12,7 @@ import reactor.core.publisher.Mono
 class CustomFilter : AbstractGatewayFilterFactory<Any>() {
 
     override fun apply(config: Any): GatewayFilter {
-        return GatewayFilter { exchange, chain ->
+        return OrderedGatewayFilter({ exchange, chain ->
             val request = exchange.request
             val response = exchange.response
 
@@ -24,7 +26,7 @@ class CustomFilter : AbstractGatewayFilterFactory<Any>() {
                         log.info { "Custom POST Filter: response code -> ${response.statusCode}" }
                     }
                 )
-        }
+        }, Ordered.HIGHEST_PRECEDENCE)
     }
 
 }
